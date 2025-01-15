0f2dc6867a4087962d0105ca20a0e27f66eb5bfc
Anamd-hub <anasmdl120@gmail.com>
2025-01-15 23:31:56 +0300

    Add: Banner section

---
+++ src/components/Banner.js
@@ -0,0 +1,21 @@
+const Banner = () => {
+  return (
+    <section className="banner">
+      <div className="banner__container">
+        <ul className="banner__list">
+          <li className="banner__item">
+            <i className="ri-leaf-line"></i>
+            <span>GOOD FOR NATURE</span>
+          </li>
+
+          <li className="banner__item">
+            <i className="ri-heart-pulse-line"></i>
+            <span>LOW CALORIES</span>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default Banner;
