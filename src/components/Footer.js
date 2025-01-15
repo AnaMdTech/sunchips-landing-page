e407e45075bcb34b4c800bccc40b75d38bc626df
Anamd-hub <anasmdl120@gmail.com>
2025-01-15 23:52:33 +0300

    Add: Footer section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,28 @@
+import Logo from "../assets/images/logo.png";
 function Footer() {
-  return <div></div>;
+  return (
+    <footer className="footer">
+      <div className="footer__container container grid">
+        <a href="/" className="footer__logo">
+          <img src={Logo} alt="logo" className="footer__img" />
+        </a>
+
+        <div className="footer__content grid">
+          <a href="/" className="footer__link">
+            Terms & Agreements
+          </a>
+          <a href="/" className="footer__link">
+            Privacy Policy
+          </a>
+
+          <span className="footer__copy">
+            &#169; All rights reserved <br /> SunChips Made by{" "}
+            <a href="/">AnaMdTech</a>.
+          </span>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
