8398ab0212eacc982b4e33f316e86b292e05305e
Anamd-hub <anasmdl120@gmail.com>
2025-01-15 23:31:41 +0300

    Add: DataBase for chips list

---
+++ src/components/ChipsData.js
@@ -0,0 +1,38 @@
+import img1 from "../assets/images/favorite-chips-1.png";
+import img2 from "../assets/images/favorite-chips-2.png";
+import img3 from "../assets/images/favorite-chips-3.png";
+import img5 from "../assets/images/sunchips-paprika-flavor.png";
+import img6 from "../assets/images/sunchips-habesha-spice-flavor.png";
+
+export const chipsList = [
+  {
+    type: "Tomato Flavor",
+    title: "SunChips",
+    price: "13",
+    img: img1,
+  },
+  {
+    type: "Tomato Flavor",
+    title: "SunChips",
+    price: "13",
+    img: img2,
+  },
+  {
+    type: "Tomato Flavor",
+    title: "SunChips",
+    price: "13",
+    img: img3,
+  },
+  {
+    type: "Habesha Spice",
+    title: "SunChips",
+    price: "13",
+    img: img5,
+  },
+  {
+    type: "Habesha Spice",
+    title: "SunChips",
+    price: "13",
+    img: img6,
+  },
+];
