1ee48074463aedacca64717622db76f786912c34
Anamd-hub <anasmdl120@gmail.com>
2025-01-16 13:14:29 +0300

    Wrap: BrowserRouter from react-dom-router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "../src/assets/styles/index.css";
+import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter
 import App from "./App";
+import "./assets/styles/index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
