e467509b8b903df359e64c791b1a6a3ff128c59e
Anamd-hub <anasmdl120@gmail.com>
2025-01-21 23:42:45 +0300

    Fix height and other formats for ProductCard

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -2,13 +2,24 @@ const ProductCard = ({ chip }) => {
   return (
     <div>
       <article className="products__card">
-        <img src={chip.img} alt="images" className="products__img" />
+        <img
+          src={chip.img}
+          alt="images"
+          className="products__img"
+          data-img={chip.img}
+        />
 
-        <span className="products__subtitle">{chip.type}</span>
-        <h2 className="products__title">{chip.title}</h2>
+        <span className="products__subtitle" data-subtitle={chip.type}>
+          {chip.type}
+        </span>
+        <h2 className="products__title" data-title={chip.title}>
+          {chip.title}
+        </h2>
 
-        <span className="products__price">{chip.price}Br</span>
-        <button className="products__button">
+        <span className="products__price" data-price={chip.price}>
+          {chip.price}Br
+        </span>
+        <button className="products__button" data-id={chip.type}>
           <i className="ri-shopping-bag-3-fill"></i>
         </button>
       </article>
