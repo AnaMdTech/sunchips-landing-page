8d4e39ee35d11b8ab70743b41acf07ddc0e6e53f
Anamd-hub <anasmdl120@gmail.com>
2025-02-20 22:38:17 +0300

    Add: animation to Products section

--- src/components/Products.js
+++ src/components/Products.js
@@ -3,25 +3,50 @@ import { chipsList } from "./ChipsData";
 import crabImg from "../assets/images/crab.png";
 import hamburgerImg from "../assets/images/hamburger.png";
 import cheeseImg from "../assets/images/cheese.png";
-// import { useState } from "react";
+import { motion } from "framer-motion";
+
+const containerVariants = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.5, // Delay each child animation by 0.5s
+    },
+  },
+};
+
+const itemVariants = {
+  hidden: { opacity: 0, y: 50 },
+  show: { opacity: 1, y: 0, transition: { duration: 1, amount: 0.5 } },
+};
 
 function Products({ handleAddToCart }) {
   return (
     <section className="products section" id="products">
-      <h2 className="section__title">
+      <motion.h1
+        initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+        viewport={{ once: true, amount: 0.3 }}
+        className="section__title"
+      >
         BEST SELLING <br /> PRODUCT
-      </h2>
+      </motion.h1>
 
       <div className="products__container container">
-        <div className="products__content grid">
+        <motion.div
+          className="products__content grid"
+          variants={containerVariants}
+          initial="hidden"
+          whileInView="show"
+          viewport={{ once: true }}
+        >
           {chipsList.map((chip, index) => (
-            <ProductCard
-              key={index}
-              chip={chip}
-              handleAddToCart={handleAddToCart}
-            />
+            <motion.div key={index} variants={itemVariants}>
+              <ProductCard chip={chip} handleAddToCart={handleAddToCart} />
+            </motion.div>
           ))}
-        </div>
+        </motion.div>
 
         <img src={crabImg} alt="images" className="products__crab" />
         <img src={hamburgerImg} alt="images" className="products__humburger" />
