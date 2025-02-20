772f2e6be7d05ac0ef6d724bb7b7b98c54914ad5
Anamd-hub <anasmdl120@gmail.com>
2025-02-20 22:48:47 +0300

    Add: animation to Footer Logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,19 @@
 import Logo from "../assets/images/logo.png";
+import { motion } from "framer-motion";
 function Footer() {
   return (
     <footer className="footer">
       <div className="footer__container container grid">
         <a href="/" className="footer__logo">
-          <img src={Logo} alt="logo" className="footer__img" />
+          <motion.img
+            initial={{ opacity: 0, scale: 0.5 }}
+            whileInView={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 1, ease: "easeOut" }}
+            viewport={{ once: true, amount: 0.5 }}
+            src={Logo}
+            alt="logo"
+            className="footer__img"
+          />
         </a>
 
         <div className="footer__content grid">
