ec9761d54195f9aad4a253148c9d3bfb8ec76c2c
Anamd-hub <anasmdl120@gmail.com>
2025-02-20 22:25:25 +0300

    Add: animation to Favorites section

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,22 +1,42 @@
 import sunchips_habesha_spice from "../assets/images/sunchips-habesha-spice-flavor.png";
 import sunchips_natural from "../assets/images/sunchips-natural-flavor.png";
 import sunchips_tomato from "../assets/images/sunchips-tomato-flavor.png";
+import { motion } from "framer-motion";
 function Favorites() {
   return (
     <div className="favorite__container section" id="favorites">
       <div className="favorite__content">
-        <h1>Discover Your Favorite Flavor</h1>
-        <p>
+        <motion.h1
+          initial={{ opacity: 0, y: 20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+          viewport={{ once: true, amount: 0.3 }}
+          className="section__title"
+        >
+          Discover Your Favorite <br /> Flavor
+        </motion.h1>
+        <motion.p
+          initial={{ opacity: 0, y: 20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.5 }}
+          viewport={{ once: true, amount: 0.3 }}
+        >
           Enjoy the authentic taste of ethiopia with SunChips! From classic
           original salt to the bold Habesha blend, our chips are crafted with
           local ingredients for a truly savory snack.
-        </p>
+        </motion.p>
       </div>
-      <div className="favorite__imgs">
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1, easing: "easeOut" }}
+        viewport={{ once: true, amount: 0.5 }}
+        className="favorite__imgs"
+      >
         <img src={sunchips_habesha_spice} alt="sunchips_habesha_spice" />
         <img src={sunchips_natural} alt="sunchips_natural" />
         <img src={sunchips_tomato} alt="sunchips_tomato" />
-      </div>
+      </motion.div>
     </div>
   );
 }
